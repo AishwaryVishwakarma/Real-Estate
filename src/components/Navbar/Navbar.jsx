485ef17889f139ee68b879637216a767f70e820c
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2022-10-06 00:44:17 +0530

    --minute changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,43 @@ import { BiSearch } from "react-icons/bi";
 import classes from "./Navbar.module.css";
 
 function Navbar() {
+  const [selectedTab, setSelectedTab] = React.useState("buy");
+
   return (
     <nav className={classes.navbar}>
       <ul className={classes.nav__items}>
         <IoHome className={classes.nav__logo} />
         <li className={classes.heading}>DreamHouse</li>
-        <li>Buy</li>
-        <li>Rent</li>
-        <li>Projects</li>
-        <li>Blogs</li>
-        <li>Contact Us</li>
+        <li
+          className={selectedTab === "buy" ? classes.selected : ""}
+          onClick={() => setSelectedTab("buy")}
+        >
+          Buy
+        </li>
+        <li
+          className={selectedTab === "rent" ? classes.selected : ""}
+          onClick={() => setSelectedTab("rent")}
+        >
+          Rent
+        </li>
+        <li
+          className={selectedTab === "projects" ? classes.selected : ""}
+          onClick={() => setSelectedTab("projects")}
+        >
+          Projects
+        </li>
+        <li
+          className={selectedTab === "blogs" ? classes.selected : ""}
+          onClick={() => setSelectedTab("blogs")}
+        >
+          Blogs
+        </li>
+        <li
+          className={selectedTab === "contact" ? classes.selected : ""}
+          onClick={() => setSelectedTab("contact")}
+        >
+          Contact Us
+        </li>
         <div className={classes.searchbox}>
           <input type="text" placeholder="Search" />
           <BiSearch />
